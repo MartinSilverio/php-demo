ea4a832faf0d5be874949b0950f7ca50f25d60e0
Martin Silverio <msilverio324@gmail.com>
2021-03-06 10:20:20 -0800

    initial files

---
+++ arrays.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+    // Older way to do it
+    // $numberList = array();
+    $numberList = [23,34,5,200,7, 'hello', '345'];
+
+    // echo $numberList[0];
+    print_r($numberList);
+
+  ?>
+</body>
+</html>
